7e8323f39e9ab82fd10c0d8b51a2ff53530bf9dc
Sabeer MZ <sabeertvc@gmail.com>
2021-03-10 12:06:33 +0000

    adding files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "<h1> Deployed to Heroku</h1>"
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
